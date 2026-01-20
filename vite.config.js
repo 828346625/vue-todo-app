9257a0e887a4436f4809a11677fd0aab525043f1
David Chen <3021564982@qq.com>
2026-01-20 16:58:15 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './'  // 这行非常重要！必须添加
+})
